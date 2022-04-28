aa900a8e1d3567259a1c87b6aa0f633559d6ffeb
sparkymannthat <simonparkinson@scpsoft.com>
2022-04-28 16:40:09 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -30,4 +30,14 @@ module "vpc" {
             subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
         }
     ]
+    secondary_ranges = {
+        subnet-01 = [
+            {
+                range_name    = "subnet-01-secondary-01"
+                ip_cidr_range = "192.168.64.0/24"
+            },
+        ]
+
+    subnet-02 = []
+    }
 }
