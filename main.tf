0b30c8ca20b6783f6794ac711f1c6aaa836cb9f4
sparkymannthat <simonparkinson@scpsoft.com>
2022-04-28 16:32:15 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -67,7 +67,9 @@ resource "google_compute_instance" "hashicat" {
   tags = ["http-server"]
 
   labels = {
-    name = "hashicat"
+    name = "hashicat",
+    department = "test",
+    billable = "yes"
   }
 
 }
